d72f7226e589b31a9420d967cabe4eab98bd4513
Christian Zosel <christian@zosel.ch>
2017-03-20 08:18:27 +0100

    Add more examples to get started

--- test.js
+++ test.js
@@ -1,7 +1,32 @@
 var test = require('tape');
-var { removeDuplicates, allTheSame } = require('./tictactoe');
+var tictactoe = require('./tictactoe');
 
-test('removeDuplicates removes all duplicates from an array', function(t) {
+var shout = tictactoe.shout;
+var sayHello = tictactoe.sayHello;
+var shoutHello = tictactoe.shoutHello;
+var removeDuplicates = tictactoe.removeDuplicates;
+var allTheSame = tictactoe.allTheSame;
+
+
+test('shout turns any string into UPPERCASE', function(t) {
+    t.equals(shout('hello'), 'HELLO', 'it should shout HELLO');
+    t.equals(shout(), null, 'if no string is given, it should return null');
+    t.equals(shout(6), null, 'if no string is given, it should return null');
+    t.end();
+});
+
+test.skip('sayHello should say hello the any given person', function(t) {
+    t.equals(sayHello('powercoders'), 'Hello powercoders!');
+    t.equals(sayHello('World'), 'Hello World!');
+    t.end();
+});
+
+test.skip('shoutHello should shout the greeting', function(t) {
+    t.equals(shoutHello('powercoders'), 'HELLO POWERCODERS!');
+    t.equals(shoutHello('World'), 'HELLO WORLD!');
+});
+
+test.skip('removeDuplicates removes all duplicates from an array', function(t) {
     t.deepEquals(removeDuplicates([1, 2, 3]), [1, 2, 3]);
     t.deepEquals(removeDuplicates([1, 1, '1']), [1, '1'], 'data types matter');
     t.deepEquals(removeDuplicates([0, 'a', null, undefined, 'a', null]), [0, 'a', null, undefined]);
--- tictactoe.js
+++ tictactoe.js
@@ -1,3 +1,16 @@
+function shout(str) {
+  // TODO implement me
+}
+
+function sayHello(person) {
+  // TODO implement me
+}
+
+function shoutHello(person) {
+  // TODO implement me
+  // maybe we can use our "shout" function from above?
+}
+
 function removeDuplicates(arr) {
   // TODO implement me
 }
@@ -7,6 +20,9 @@ function allTheSame(arr) {
 }
 
 module.exports = {
+  shout,
+  sayHello,
+  shoutHello,
   removeDuplicates,
   allTheSame
 }
